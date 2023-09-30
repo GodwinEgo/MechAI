204851b3ead2817f2988278550bcb89c6b82ae48
GodwinEgo <godwinehile04@gmail.com>
2023-09-30 01:18:16 +0100

    files

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const dotenv = require ("dotenv")
+dotenv.config();
+const cors = require("cors")
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 5001;
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+//CONNECT MONGODB
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Connected to mongodb')
+}).catch((error)=>{
+    console.error(error)
+})
+
+app.get('/', (req, res)=>{
+    res.send({message:"new project"})
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`App is listening on ${port}`)
+})
